51407cae8855888e56af3eecee9843e41ff330d7
amirsh87 <amirshafiei87@yahoo.com>
2021-05-29 12:33:35 +0430

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+
+setup(name='fixer_demo',
+      version='0.1',
+      description='Fixer service demo package',
+      url='',
+      author='Amir',
+      author_email='a.h.mirshafiei@gmail.com',
+      license='MIT',
+      packages=['fixer'],
+      zipsafe=False)
